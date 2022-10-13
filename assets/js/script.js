83a2ac88049acac870787106d3003a9f9f74b41e
Nilton Duarte <tvirapegubeco@gmail.com>
2022-10-13 02:49:25 -0300

    Ajustes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,10 @@
 const cells = document.querySelectorAll('.hash-container div');
 const modal = document.querySelector('dialog');
 const restart = document.querySelector('button.restart');
+const pTurn = document.querySelector('p.turn');
 let check;
+
+modal.querySelector('button').onclick = () => modal.close();
 
 let play1 = '';
 
@@ -24,6 +27,7 @@ function toPlay() {
         return
     let play2 = play1 === 'O' ? 'X' : 'O';
     let play = play1;
+    pTurn.textContent = `Vez da jogada: ${play}`;
     let hash = [];
     for (let i = 0; i < cells.length; i++) {
         hash.push('');
@@ -43,17 +47,23 @@ function toPlay() {
                 modal.showModal();
                 modal.querySelector('p').innerHTML = 
                     `${play === 'X' ? 'O' : 'A' } <b style="color: blue;">${play}</b> ganhou!`;
-                modal.querySelector('button').onclick = () => modal.close();
-
-            } else if (win === null) {
+                pTurn.textContent = '';
+            } else {
                 cell.textContent = play;
                 play = play === 'O' ? 'X' : 'O';
+                pTurn.textContent = `Vez da jogada: ${play}`;
+            }
+            if (win === null && !hash.includes('')) {
+                modal.showModal();
+                modal.querySelector('p').innerHTML = 'Empate!';
+                pTurn.textContent = '';
             }
             cell.removeEventListener('click', check);
         };
         cell.addEventListener('click', check);
     });
 }
+
 function verifyWin(hash, opt) {
     if (hash[0] === opt && hash[1] === opt && hash[2] === opt) {
         return [0, 1, 2];
